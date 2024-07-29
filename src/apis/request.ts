840e7d3f6ff0ba91fdaf3bde5a4a8b16105694cc
luolananDR <1525816996@qq.com>
2024-07-29 12:53:03 +0800

    Initial commit

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -4,6 +4,7 @@ const request = async <TData, TProps = any>(
   config: AxiosRequestConfig<TProps>
 ) => {
   const res = await axios<TData>({
+    baseURL: "http://localhost:5173/",
     timeout: 10000,
     ...config
   });
