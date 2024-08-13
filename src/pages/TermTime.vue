7dcf8681dbfa8424197cd8985c76b3108179cc4b
xixiibn5100 <1584914306@qq.com>
2024-08-14 00:06:47 +0800

    fix(Tips):修复新生提醒页面不显示后段数据的bug

--- src/pages/TermTime.vue
+++ src/pages/TermTime.vue
@@ -8,78 +8,36 @@ import {
   NAlert,
   NSpace,
   NDatePicker,
-  useDialog,
+  useDialog, useMessage,
 } from "naive-ui";
 import getSystemInfo from "../apis/SystemInfoAPI/getSystemInfo";
 import setTermInfoAPI from "../apis/TermTimeAPI/setTermInfo";
 import PageTitle from "../components/PageTitle.vue";
-
-const initialValue = ref({
-  termYear: new Date().getFullYear().toString(),
-  term: "上",
-  termStartDate: "1970-01-01",
-  scoreTerm: "上",
-  scoreYear: new Date().getFullYear().toString(),
-  schoolBusUrl: "暂无",
-  jpgUrl: "暂无",
-  fileUrl: "暂无",
-  registerTips: "暂无",
-});
-
-const termYearValue = ref(initialValue.value.termYear);
-const termValue = ref(initialValue.value.term);
-const termStartDateValue = ref(initialValue.value.termStartDate);
-const scoreTermValue = ref(initialValue.value.scoreTerm);
-const scoreYearValue = ref(initialValue.value.scoreYear);
-const schoolBusUrlValue = ref(initialValue.value.schoolBusUrl);
-const jpgUrlValue = ref(initialValue.value.jpgUrl);
-const fileUrlValue = ref(initialValue.value.fileUrl);
-const registerTips = ref(initialValue.value.registerTips);
-const dialog = useDialog();
-
-const handleReset = async () => {
-  termYearValue.value = initialValue.value.termYear,
-  termValue.value = initialValue.value.term,
-  termStartDateValue.value = initialValue.value.termStartDate,
-  scoreTermValue.value = initialValue.value.scoreTerm,
-  scoreYearValue.value = initialValue.value.scoreYear;
-};
-
-const noticeReset = async () => {
-  registerTips.value=initialValue.value.registerTips;
-};
-
-const urlReset = async () => {
-  schoolBusUrlValue.value = initialValue.value.schoolBusUrl,
-  jpgUrlValue.value = initialValue.value.jpgUrl,
-  fileUrlValue.value = initialValue.value.fileUrl;
-};
-
-
+const message = useMessage();
+const initialValue = ref();
 
 onMounted(async () => {
   try {
     const { code, data, msg } = await getSystemInfo();
     if (code !== 1) throw new Error(msg);
     const { term, termStartDate, termYear,scoreTerm,scoreYear,schoolBusUrl,jpgUrl,fileUrl,registerTips } = data;
-    initialValue.value.termYear = termYear;
-    initialValue.value.term = term;
-    initialValue.value.termStartDate = termStartDate;
-    initialValue.value.scoreTerm = scoreTerm;
-    initialValue.value.scoreYear = scoreYear;
-    initialValue.value.schoolBusUrl = schoolBusUrl;
-    initialValue.value.jpgUrl = jpgUrl;
-    initialValue.value.fileUrl = fileUrl;
-    initialValue.value.registerTips = registerTips;
+      initialValue.value = {
+      termYear: termYear,
+      term: term,
+      termStartDate: termStartDate,
+      scoreTerm: scoreTerm,
+      scoreYear: scoreYear,
+      schoolBusUrl: schoolBusUrl,
+      jpgUrl: jpgUrl,
+      fileUrl: fileUrl,
+      registerTips: registerTips,
+    };
   } catch (e) {
     console.log(e);
   }
-  urlReset();
-  noticeReset();
-  //handleReset();
 });
 //this.$forceUpdate()
-
+const dialog = useDialog();
 
 const optionsTerm = [
   { label: "上", value: "上" },
@@ -92,24 +50,25 @@ const optionsTerm = [
 const handleSubmit = async () => {
   dialog.warning({
     title: "警告",
-    content: `确认修改学期信息为: ${termYearValue.value}学年${termValue.value}学期 / ${termStartDateValue.value}`,
+    content: `确认修改学期信息为: ${initialValue.value.termYear}学年${initialValue.value.term}学期 / ${initialValue.value.termStartDate}`,
     positiveText: "确定",
     negativeText: "回去改一下",
     onPositiveClick: async () => {
       try {
         const res = await setTermInfoAPI({
-          yearValue: termYearValue.value,
-          termValue: termValue.value,
-          termStartDateValue: termStartDateValue.value,
-          scoreTermValue:scoreTermValue.value,
-          scoreYearValue:scoreYearValue.value,
-          jpgUrlValue: jpgUrlValue.value,
-          fileUrlValue: fileUrlValue.value,
-          schoolBusUrlValue:schoolBusUrlValue.value,
-          registerTips:registerTips.value,
+          yearValue: initialValue.value.termYear,
+          termValue: initialValue.value.term,
+          termStartDateValue: initialValue.value.termStartDate,
+          scoreTermValue:initialValue.value.scoreTerm,
+          scoreYearValue:initialValue.value.scoreYear,
+          jpgUrlValue: initialValue.value.jpgUrl,
+          fileUrlValue: initialValue.value.fileUrl,
+          schoolBusUrlValue: initialValue.value.schoolBusUrl,
+          registerTips: initialValue.value.registerTips,
         });
         const { code, msg } = res;
         if (code !== 1) throw new Error(msg);
+        message.success("修改成功");
       } catch (e) {
         console.log(e);
       }
@@ -117,41 +76,54 @@ const handleSubmit = async () => {
   });
 };
 
+const submit = async  () => {
+  try {
+    const res = await setTermInfoAPI({
+      yearValue: initialValue.value.termYear,
+      termValue: initialValue.value.term,
+      termStartDateValue: initialValue.value.termStartDate,
+      scoreTermValue:initialValue.value.scoreTerm,
+      scoreYearValue:initialValue.value.scoreYear,
+      jpgUrlValue: initialValue.value.jpgUrl,
+      fileUrlValue: initialValue.value.fileUrl,
+      schoolBusUrlValue: initialValue.value.schoolBusUrl,
+      registerTips: initialValue.value.registerTips,
+    });
+    const { code, msg } = res;
+    if (code !== 1) throw new Error(msg);
+    message.success("修改成功");
+  } catch (e) {
+    message.error(e);
+  }
+}
+
 </script>
 
 <template>
   <page-title>学期时间编辑</page-title>
   <n-space style="padding: 0 24px">
 
-  <n-form style="max-width: 400px">
-    <n-form-item>
-      <n-alert type="info" style="width: 400px">
-        表单的初始值为当前系统的数据，刷新以查看
-      </n-alert>
-    </n-form-item>
+  <n-form style="max-width: 400px" v-if="initialValue">
     <n-form-item label="学年选择">
-      <n-date-picker v-model:formatted-value="termYearValue" value-format="yyyy" type="year" style="width: 400px" />
+      <n-date-picker v-model:formatted-value="initialValue.termYear" value-format="yyyy" type="year" style="width: 400px" />
     </n-form-item>
     <n-form-item label="学期选择">
-      <n-select v-model:value="termValue" :options="optionsTerm" clearable />
+      <n-select v-model:value="initialValue.term" :options="optionsTerm" clearable />
     </n-form-item>
     <n-form-item label="学期开始时间选择：">
-      <n-date-picker v-model:formatted-value="termStartDateValue" value-format="yyyy-MM-dd" type="date"
+      <n-date-picker v-model:formatted-value="initialValue.termStartDate" value-format="yyyy-MM-dd" type="date"
         style="width: 400px" />
     </n-form-item>
     <n-form-item label="成绩查询学年选择">
-      <n-date-picker v-model:formatted-value="scoreYearValue" value-format="yyyy" type="year" style="width: 400px" />
+      <n-date-picker v-model:formatted-value="initialValue.scoreYear" value-format="yyyy" type="year" style="width: 400px" />
     </n-form-item>
     <n-form-item label="成绩查询学期选择">
-      <n-select v-model:value="scoreTermValue" :options="optionsTerm" clearable />
+      <n-select v-model:value="initialValue.scoreTerm" :options="optionsTerm" clearable />
     </n-form-item>
     <n-form-item>
       <n-space>
         <n-button id="update" type="success" @click="handleSubmit">
           提交修改
-        </n-button>
-        <n-button secondary type="info" id="clear" @click="handleReset">
-          刷新表单
         </n-button>
       </n-space>
     </n-form-item>
@@ -161,27 +133,20 @@ const handleSubmit = async () => {
   <page-title>新生注册提醒</page-title>
   <n-space style="padding: 0 24px">
 
-  <n-form style="max-width: 400px">
-  <n-form-item>
-    <n-alert type="info" style="width: 400px">
-      表单的初始值为当前系统的数据，刷新以查看
-    </n-alert>
-  </n-form-item>
+  <n-form style="max-width: 400px" v-if="initialValue">
   <n-form-item>
     <n-input
-      v-model:value="registerTips"
+      v-model:value="initialValue.registerTips"
       type="textarea"
       placeholder="请输入新生注册提醒"
+      style="width: 400px"
     />
   </n-form-item>
 
   <n-form-item>
     <n-space>
-      <n-button id="update" type="success" @click="handleSubmit">
+      <n-button id="update" type="success" @click="submit">
         发布提醒
-      </n-button>
-      <n-button secondary type="info" id="clear" @click="noticeReset">
-        刷新表单
       </n-button>
     </n-space>
   </n-form-item>
@@ -190,18 +155,14 @@ const handleSubmit = async () => {
   <page-title>链接编辑</page-title>
   <n-space style="padding: 0 24px">
 
-<n-form style="max-width: 400px">
-<n-form-item>
-  <n-alert type="info" style="width: 400px">
-    表单的初始值为当前系统的数据，刷新以查看
-  </n-alert>
-</n-form-item>
+<n-form style="max-width: 400px" v-if="initialValue">
 <n-form-item >
   <n-card title="图片链接" size="small" :bordered="false">
     <n-input
-    v-model:value="jpgUrlValue"
-    type="input"
+    v-model:value="initialValue.jpgUrl"
+    type="text"
     placeholder="请输入图片URL"
+    style="width: 400px"
   />
   </n-card>
 
@@ -209,29 +170,28 @@ const handleSubmit = async () => {
 <n-form-item >
   <n-card title="文件链接" size="small" :bordered="false">
     <n-input
-    v-model:value="fileUrlValue"
-    type="input"
+    v-model:value="initialValue.fileUrl"
+    type="text"
     placeholder="请输入文件URL"
+    style="width: 400px"
   />
   </n-card>
 </n-form-item>
 <n-form-item >
   <n-card title="校车链接" size="small" :bordered="false">
   <n-input
-    v-model:value="schoolBusUrlValue"
-    type="input"
+    v-model:value="initialValue.schoolBusUrl"
+    type="text"
     placeholder="请输入校车URL"
+    style="width: 400px"
   />
 </n-card>
 </n-form-item>
 
 <n-form-item>
   <n-space>
-    <n-button id="update" type="success" @click="handleSubmit">
+    <n-button id="update" type="success" @click="submit">
       修改前缀
-    </n-button>
-    <n-button secondary type="info" id="clear" @click="urlReset">
-      刷新表单
     </n-button>
   </n-space>
 </n-form-item>
